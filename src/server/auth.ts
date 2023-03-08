eabc9f21ad8f3bac0fb27dba1b76c19b8f60bc8f
SantiagoVira <santiagovira06@gmail.com>
2023-03-07 19:48:32 -0500

    types

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -1,34 +1,9 @@
 import { type GetServerSidePropsContext } from "next";
-import {
-  getServerSession,
-  type NextAuthOptions,
-  type DefaultSession,
-} from "next-auth";
+import { getServerSession, type NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { env } from "~/env.mjs";
 import { prisma } from "~/server/db";
-
-/**
- * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
- * object and keep type safety.
- *
- * @see https://next-auth.js.org/getting-started/typescript#module-augmentation
- */
-declare module "next-auth" {
-  interface Session extends DefaultSession {
-    user: {
-      id: string;
-      // ...other properties
-      // role: UserRole;
-    } & DefaultSession["user"];
-  }
-
-  // interface User {
-  //   // ...other properties
-  //   // role: UserRole;
-  // }
-}
 
 /**
  * Options for NextAuth.js used to configure adapters, providers, callbacks, etc.
@@ -40,6 +15,9 @@ export const authOptions: NextAuthOptions = {
     session({ session, user }) {
       if (session.user) {
         session.user.id = user.id;
+        session.user.board = user.board;
+        session.user.roomId = user.roomId;
+        session.user.room = user.room;
         // session.user.role = user.role; <-- put other properties on the session here
       }
       return session;
---
+++ src/types/next-auth.d.ts
@@ -0,0 +1,24 @@
+import type { Board, User as PrismaUser, Room } from "@prisma/client";
+
+/**
+ * Module augmentation for `next-auth` types.
+ * Allows us to add custom properties to the `session` object and keep type
+ * safety.
+ *
+ * @see https://next-auth.js.org/getting-started/typescript#module-augmentation
+ **/
+
+declare module "next-auth" {
+  /**
+   * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
+   */
+  interface Session {
+    user: User;
+  }
+
+  interface User extends PrismaUser {
+    room: Room?;
+    roomId: string?;
+    board: Board?;
+  }
+}
