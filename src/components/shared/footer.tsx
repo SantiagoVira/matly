1f3505f40b5d5442c7dc591b2afffb160db7f3fa
SantiagoVira <santiagovira06@gmail.com>
2023-03-09 19:30:54 -0500

    rebrand

--- src/components/shared/footer.tsx
+++ src/components/shared/footer.tsx
@@ -5,12 +5,7 @@ const Footer: React.FC = () => {
     <div className="mt-6 flex h-[10rem] w-screen items-center justify-between bg-bg-200 px-20 shadow">
       <div className="flex gap-4">
         <Image src="/logo.svg" alt="" width={65} height={10} />
-        <div className="flex h-full flex-col justify-between">
-          <h1 className="text-[2rem] tracking-tight text-text-100">
-            Neighbors
-          </h1>
-          <p className="text-text-200">Because digital is better than paper</p>
-        </div>
+        <h1 className="text-2xl tracking-tight text-text-100">Matly</h1>
       </div>
     </div>
   );
