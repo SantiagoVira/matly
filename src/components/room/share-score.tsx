25b8907d564ff5b96b390c9fb94fe3e9c855bb50
SantiagoVira <santiagovira06@gmail.com>
2023-03-12 19:44:26 -0400

    make it send the damn link

--- src/components/room/share-score.tsx
+++ src/components/room/share-score.tsx
@@ -12,7 +12,7 @@ const ShareScore: React.FC<{ score: number | undefined }> = ({ score }) => {
             title: "Daily Matly",
             text: `I scored ${
               score ?? 0
-            } on today's daily Matly. Can you beat me?`,
+            } on today's daily Matly. Can you beat me? https://matly.vercel.app/daily`,
             url: "https://matly.vercel.app/daily",
           });
         } else {
