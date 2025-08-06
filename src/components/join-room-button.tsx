4113647a4626f8fe1649ccd0e2cf13badc780a6b
SantiagoVira <santiagovira06@gmail.com>
2025-08-05 23:55:17 -0400

    Move join room functionality

---
+++ src/components/join-room-button.tsx
@@ -0,0 +1,89 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
+import Button from "~/components/ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "~/components/ui/dialog";
+import { Input } from "~/components/ui/input";
+import HomeScreenButton from "./home-screen-button";
+import JoinRoomIcon from "./icons/join-room";
+import { cn } from "~/utils/cn";
+import { api } from "~/utils/api";
+import { useState } from "react";
+import { useRouter } from "next/router";
+
+export function JoinRoomButton() {
+  const router = useRouter();
+  const ctx = api.useContext();
+  const [error, setError] = useState("");
+  const [joinCode, setJoinCode] = useState("");
+  const roomExists = api.room.findUnique.useQuery({
+    id: joinCode,
+  });
+
+  const joinMutation = api.room.join.useMutation({
+    onSuccess: async (e) => {
+      await ctx.invalidate();
+      document.dispatchEvent(new Event("visibilitychange"));
+      await router.push(`/room/${e[0].id}`);
+    },
+  });
+
+  const onJoin = async () => {
+    await roomExists.refetch();
+    if (!roomExists.data) {
+      setError("Room not Found");
+      return;
+    }
+    if (roomExists.data.playing) {
+      setError("Room currently playing");
+      return;
+    }
+    await joinMutation.mutateAsync({ id: joinCode });
+  };
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <HomeScreenButton
+          className="col-span-3 col-start-4 row-span-2"
+          Icon={JoinRoomIcon}
+          onClick={() => console.log("Open dialog")}
+          title="Join room"
+        />
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-md">
+        <DialogHeader>
+          <DialogTitle>Enter join code</DialogTitle>
+          <DialogDescription>
+            Ask your friends for the big code on their screens!
+          </DialogDescription>
+        </DialogHeader>
+        <div className="flex flex-col items-start">
+          <Input
+            className={cn("h-10 w-64 ", error && "border border-rose-600")}
+            onKeyDown={async (e) => {
+              if (e.key === "Enter") {
+                await onJoin();
+              }
+            }}
+            onChange={(e) => setJoinCode(e.target.value.toLowerCase())}
+          />
+
+          {error && <p className="text-sm text-rose-600">{error}</p>}
+        </div>
+        <DialogFooter className="justify-end">
+          <DialogClose asChild>
+            <Button type="button">Close</Button>
+          </DialogClose>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
--- src/components/no-room-home.tsx
+++ src/components/no-room-home.tsx
@@ -1,27 +1,16 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
 import { useRouter } from "next/router";
 import { api } from "~/utils/api";
-import clsx from "clsx";
-import { Input } from "./ui/input";
-import { useState } from "react";
-import useWindowSize from "~/utils/useWindowSize";
 import HomeScreenButton from "./home-screen-button";
 import CreateRoomIcon from "./icons/create-room";
-import JoinRoomIcon from "./icons/join-room";
 import SingleplayerIcon from "./icons/singleplayer";
 import DailyIcon from "./icons/daily";
 import HowToIcon from "./icons/how-to";
+import { JoinRoomButton } from "./join-room-button";
 
 const NoRoomHome: React.FC = () => {
   const router = useRouter();
   const ctx = api.useContext();
-
-  const [joinCode, setJoinCode] = useState("");
-  const [error, setError] = useState("");
-  const roomExists = api.room.findUnique.useQuery({
-    id: joinCode,
-  });
-  const { isMobile } = useWindowSize();
 
   const createMutation = api.room.create.useMutation({
     onSuccess: async (e) => {
@@ -30,45 +19,9 @@ const NoRoomHome: React.FC = () => {
       await router.push(`/room/${e[0].id}`);
     },
   });
-  const joinMutation = api.room.join.useMutation({
-    onSuccess: async (e) => {
-      await ctx.invalidate();
-      document.dispatchEvent(new Event("visibilitychange"));
-      await router.push(`/room/${e[0].id}`);
-    },
-  });
-
-  const onJoin = async () => {
-    await roomExists.refetch();
-    if (!roomExists.data) {
-      setError("Room not Found");
-      return;
-    }
-    if (roomExists.data.playing) {
-      setError("Room currently playing");
-      return;
-    }
-    await joinMutation.mutateAsync({ id: joinCode });
-  };
 
   return (
     <>
-      <div className="flex flex-col items-center">
-        <Input
-          className={clsx(
-            "h-10 w-[7.5rem] md:w-32",
-            error && "border border-rose-600"
-          )}
-          onKeyDown={async (e) => {
-            if (e.key === "Enter") {
-              await onJoin();
-            }
-          }}
-          onChange={(e) => setJoinCode(e.target.value.toLowerCase())}
-        />
-
-        {error && <p className="text-sm text-rose-600">{error}</p>}
-      </div>
       <div className="grid w-full max-w-5xl grid-cols-6 grid-rows-4 gap-2">
         <HomeScreenButton
           className="col-span-3 row-span-2"
@@ -76,12 +29,7 @@ const NoRoomHome: React.FC = () => {
           onClick={() => createMutation.mutateAsync()}
           title="Create room"
         />
-        <HomeScreenButton
-          className="col-span-3 col-start-4 row-span-2"
-          Icon={JoinRoomIcon}
-          onClick={onJoin}
-          title="Join room"
-        />
+        <JoinRoomButton />
         <HomeScreenButton
           className="col-span-2 row-span-2 row-start-3"
           Icon={SingleplayerIcon}
